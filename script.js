0f573aeca906ea4adf25552dc37b8d35a33b92a8
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-08-03 14:32:10 +0500

    updated

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.querySelector("#gen-text");
+const text =
+  "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890";
+
+function generatePassword() {
+  let generated = "";
+  for (let i = 0; i < 16; i++) {
+    const random = Math.floor(Math.random() * text.length);
+    generated += text.charAt(random);
+  }
+  input.value = generated;
+}
+
+function copyToClipboard() {
+  input.select();
+  document.execCommand("copy");
+}
