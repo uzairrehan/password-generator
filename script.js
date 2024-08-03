fe5359657396255a66156fb5a47c387f5b53b8d0
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-08-03 14:57:33 +0500

    Added IIFE

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const input = document.querySelector("#gen-text");
 const text =
   "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890";
 
-function generatePassword() {
+(function generatePassword() {
   let generated = "";
   for (let i = 0; i < 16; i++) {
     const random = Math.floor(Math.random() * text.length);
     generated += text.charAt(random);
   }
   input.value = generated;
-}
+})()
 
 function copyToClipboard() {
   input.select();
